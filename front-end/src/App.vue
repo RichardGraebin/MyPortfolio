cb7a4208b32cc876a0f804a37f88fbc90e9a067d
richard <richardgraebinjs@gmail.com>
2022-06-12 02:07:57 -0300

    fix: import NavBar into main application

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,32 +1,32 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+    <NavBar></NavBar>
   </div>
 </template>
 
 <style lang="scss">
+* {
+  margin: 0;
+  background: #212529;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
+<script lang="ts">
+// Dependencies
+import Vue from "vue";
+import Component from "vue-class-component";
+
+// Components
+import NavBar from "./components/NavBar.vue";
+
+@Component({
+  components: {
+    NavBar,
+  },
+})
+export default class App extends Vue {}
+</script>
