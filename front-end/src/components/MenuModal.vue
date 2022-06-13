4a6fd440af123ab0fa8efe23dc1a283124608f74
richard <richardgraebinjs@gmail.com>
2022-06-12 22:58:49 -0300

    feat: created menu base

---
+++ front-end/src/components/MenuModal.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="menu-container">
+    <div class="menu__">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>What i know</li>
+        <li>Contact</li>
+        <li>More</li>
+      </ul>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+.menu-container {
+  position: fixed;
+  background: red;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
+  .menu__ {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class MenuModal extends Vue {}
+</script>
